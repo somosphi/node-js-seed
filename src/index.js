df7da60894f916243bf3f8901438fb5de6a3eda9
olavio.lacerda <olavio.lacerda@4all.com>
2019-12-03 10:53:50 -0300

    removed eureka from configs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const apm = require('elastic-apm-node');
-const { Eureka } = require('eureka-js-client');
 const env = require('./env');
 const http = require('./http');
 const worker = require('./worker');
@@ -16,45 +15,6 @@ setImmediate(() => {
       serviceName: env.SERVICE_URL,
       secretToken: env.APM_TOKEN,
       serverUrl: env.APM_URL,
-    });
-  }
-
-  if (env.NODE_ENV === 'production') {
-    const eureka = new Eureka({
-      instance: {
-        instanceId: `${env.HOSTNAME}:${env.SERVICE_URL}:${env.PORT}`,
-        healthCheckUrl: `http://${env.SERVICE_URL}:${env.PORT}/status`,
-        app: env.SERVICE_NAME,
-        hostName: env.SERVICE_URL,
-        ipAddr: '',
-        statusPageUrl: `http://${env.SERVICE_URL}:${env.PORT}/info`,
-        vipAddress: env.SERVICE_NAME,
-        port: {
-          $: env.PORT,
-          '@enabled': true,
-        },
-        dataCenterInfo: {
-          '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-          name: 'MyOwn',
-        },
-        registerWithEureka: true,
-        fetchRegistry: true,
-      },
-      eureka: {
-        host: env.EUREKA_HOST,
-        port: env.EUREKA_PORT,
-        servicePath: '/service-discovery/eureka/apps/',
-        maxRetries: 20,
-        requestRetryDelay: 1000,
-      },
-    });
-
-    eureka.start((err) => {
-      if (err) {
-        logger.error(__('eureka.error', err));
-      } else {
-        logger.info(__('eureka.started'));
-      }
     });
   }
 
