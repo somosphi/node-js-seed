e0f5f7607b651a0b6446d0930b6b99dcc5782494
olavio.lacerda <olavio.lacerda@4all.com>
2019-12-05 09:51:07 -0300

    :sparkles: added http-errors lib

--- src/errors.js
+++ src/errors.js
@@ -1,75 +1,26 @@
-class CodedError extends Error {
-  /**
-   * @param {String} code
-   */
-  constructor(code) {
-    super();
-    this.code = code;
-  }
+const {
+  BadRequest,
+  InternalServerError,
+  NotFound,
+  NotImplemented,
+  Unauthorized,
+  UnprocessableEntity,
+  CustomError,
+} = require('@4alltecnologia/http-errors');
 
-  /**
-   * @param {String} message
-   */
-  setMessage(message) {
-    this.message = message;
-    return this;
-  }
-
-  toJSON() {
-    return {
-      message: this.message || null,
-      code: this.code || null,
-    };
-  }
-}
-
-class DetailedCodedError extends CodedError {
-  /**
-   * @param {*} details
-   */
-  setDetails(details) {
-    this.details = details;
-    return this;
-  }
-
-  toJSON() {
-    return {
-      ...super.toJSON(),
-      details: this.details || null,
-    };
-  }
-}
-
-class NotFoundError extends CodedError {
-  constructor() {
-    super('NOT_FOUND');
-  }
-}
-
-class ResourceNotFoundError extends CodedError {
-  constructor() {
-    super('RESOURCE_NOT_FOUND');
-  }
-}
-
-class ResourceDuplicatedError extends CodedError {
-  constructor() {
-    super('RESOURCE_DUPLICATED');
-  }
-}
-
-class ValidationError extends DetailedCodedError {
-  constructor(details) {
-    super('VALIDATION_FAILED');
-    this.setDetails(details);
+class Conflict extends CustomError {
+  constructor(message, details) {
+    super('CONFLICT', message, details);
   }
 }
 
 module.exports = {
-  CodedError,
-  DetailedCodedError,
-  NotFoundError,
-  ResourceNotFoundError,
-  ResourceDuplicatedError,
-  ValidationError,
-};
+  BadRequest,
+  InternalServerError,
+  NotFound,
+  NotImplemented,
+  Unauthorized,
+  UnprocessableEntity,
+  CustomError,
+  Conflict
+}
--- src/http/middlewares/errorHandler.js
+++ src/http/middlewares/errorHandler.js
@@ -1,83 +1,48 @@
 const {
-  CodedError,
-  NotFoundError,
-  ResourceNotFoundError,
-  ValidationError,
+  CustomError,
+  NotFound,
+  Unauthorized,
+  BadRequest,
+  InternalServerError,
+  Conflict,
 } = require('../../errors');
-
 const { logger } = require('../../logger');
 
-/**
- * @typedef ErrorConfig
- * @type {Object}
- * @property {typeof CodedError} class
- * @property {String} i18n
- */
+// eslint-disable-next-line no-unused-vars
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let status = 500;
+  let throwErr = err;
 
-/**
- * @type {ErrorConfig[]}
- */
-const errorsConfigs = [
-  { class: NotFoundError, i18n: 'error.notFound' },
-  { class: ResourceNotFoundError, i18n: 'error.resourceNotFound' },
-  { class: ValidationError, i18n: 'error.validation' },
-];
-
-/**
- * @param {Error} error
- */
-const getErrorConfig = error => errorsConfigs
-  .find(errorConfig => error instanceof errorConfig.class);
-
-/**
- * @param {import('express').Request} req
- * @param {CodedError} error
- */
-const loadErrorMessage = (req, error) => {
-  const errorConfig = getErrorConfig(error);
-  if (errorConfig) {
-    error.setMessage(req.__(errorConfig.i18n));
+  if (throwErr instanceof BadRequest) {
+    status = 400;
   }
-};
 
-/**
- * @param {*} err
- * @param {import('express').Request} req
- * @param {import('express').Response} res
- * @param {import('express').NextFunction} next
- */
-const errorHandler = (err, req, res, next) => {
-  if (err instanceof CodedError) {
-    loadErrorMessage(req, err);
+  if (throwErr instanceof Unauthorized) {
+    status = 401;
+  }
+
+  if (err instanceof NotFound) {
+    status = 404;
+  }
+
+  if (err.code && err.code === 'ER_DUP_ENTRY') {
+    status = 409;
+    throwErr = new Conflict('Already exists resource with received unique keys');
+  }
+
+  if (!(throwErr instanceof CustomError)) {
+    throwErr = new InternalServerError(throwErr.message);
+  }
+
+  if (status !== 500) {
     logger.warn(err);
   } else {
     logger.error(err);
   }
 
-  if (err instanceof NotFoundError || err instanceof ResourceNotFoundError) {
-    res.status(404).send(err);
-    return next();
-  }
-
-  if (err instanceof ValidationError) {
-    res.status(400).send(err);
-    return next();
-  }
-
-  if (err.code && err.code === 'ER_DUP_ENTRY') {
-    res.status(409).send({
-      code: 'DUPLICATED_RESOURCE',
-      message: res.__('error.duplicatedResource'),
-    });
-    return next();
-  }
-
-  res.status(500).send({
-    code: 'UNEXPECTED_ERROR',
-    message: res.__('error.unexpected'),
-  });
-
-  return next();
+  return res
+    .status(status)
+    .send(throwErr.toJSON());
 };
 
-module.exports = errorHandler;
+module.exports = errorHandlerMiddleware;
